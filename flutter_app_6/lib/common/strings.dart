7ff0e4c915a391bb80e34f23f783d4fe2d56b09a
eduhoratiu <dev@eduhoratiu.com>
2023-11-28 11:08:33 +0200

    Add tabs to home screen

--- flutter_app_6/lib/common/strings.dart
+++ flutter_app_6/lib/common/strings.dart
@@ -1,1 +1,3 @@
 const String appTitle = 'Flutter App 6';
+
+const String homeScreenTitle = 'Home Screen';
--- flutter_app_6/lib/screens/home_screen.dart
+++ flutter_app_6/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../common/strings.dart' as strings;
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,6 +12,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(strings.homeScreenTitle),
+          bottom: TabBar(
+            tabs: [
+              Tab(),
+              Tab(),
+              Tab(),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ColoredBox(color: Colors.red),
+            ColoredBox(color: Colors.green),
+            ColoredBox(color: Colors.blue),
+          ],
+        ),
+      ),
+    );
   }
 }
