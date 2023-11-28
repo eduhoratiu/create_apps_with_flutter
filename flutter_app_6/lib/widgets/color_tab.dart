3122e800a225b94ed8e475ed97967bf180ab48b1
eduhoratiu <dev@eduhoratiu.com>
2023-11-28 11:41:30 +0200

    Add ColorTab code documentation

--- flutter_app_6/lib/widgets/color_tab.dart
+++ flutter_app_6/lib/widgets/color_tab.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// A tab that displays a color.
 class ColorTab extends StatelessWidget {
+  /// Creates a tab that displays a color. The [color] parameter is required.
+  /// The [width] and [height] parameters are optional. If not specified,
+  /// they default to 40.0.
   const ColorTab({
     super.key,
     required this.color,
@@ -8,10 +12,14 @@ class ColorTab extends StatelessWidget {
     this.height = 40,
   });
 
+  /// The color of the tab. This is a required parameter and it is used to
+  /// determine the color of the tab.
   final Color color;
 
+  /// The width of the tab. If not specified, defaults to 40.0.
   final double? width;
 
+  /// The height of the tab. If not specified, defaults to 40.0.
   final double? height;
 
   @override
