b2d98562f0c812bbc160919ca90d2515bb966b6b
eduhoratiu <dev@eduhoratiu.com>
2023-11-28 10:58:32 +0200

    Create app 6

---
+++ flutter_app_6/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'common/strings.dart' as strings;
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const FlutterApp6());
+}
+
+class FlutterApp6 extends StatelessWidget {
+  const FlutterApp6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: strings.appTitle,
+      home: HomeScreen(),
+    );
+  }
+}
