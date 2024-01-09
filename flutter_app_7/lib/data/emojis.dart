13d9936b1593841045f7e0b081503ae174afce72
eduhoratiu <dev@eduhoratiu.com>
2024-01-09 10:39:16 +0200

    Add emojis

---
+++ flutter_app_7/lib/data/emojis.dart
@@ -0,0 +1,37 @@
+class Emoji {
+  final String value;
+  final String description;
+
+  const Emoji({required this.value, required this.description});
+}
+
+const List<Emoji> emojis = [
+  Emoji(value: '😀', description: 'Grinning Face'),
+  Emoji(value: '😃', description: 'Grinning Face with Big Eyes'),
+  Emoji(value: '😄', description: 'Grinning Face with Smiling Eyes'),
+  Emoji(value: '😁', description: 'Beaming Face with Smiling Eyes'),
+  Emoji(value: '😆', description: 'Grinning Squinting Face'),
+  Emoji(value: '😅', description: 'Grinning Face with Sweat'),
+  Emoji(value: '🤣', description: 'Rolling on the Floor Laughing'),
+  Emoji(value: '😂', description: 'Face with Tears of Joy'),
+  Emoji(value: '🙂', description: 'Slightly Smiling Face'),
+  Emoji(value: '🙃', description: 'Upside-Down Face'),
+  Emoji(value: '😉', description: 'Winking Face'),
+  Emoji(value: '😊', description: 'Smiling Face with Smiling Eyes'),
+  Emoji(value: '😇', description: 'Smiling Face with Halo'),
+  Emoji(value: '🥰', description: 'Smiling Face with Hearts'),
+  Emoji(value: '😍', description: 'Smiling Face with Heart-Eyes'),
+  Emoji(value: '🤩', description: 'Star-Struck'),
+  Emoji(value: '😘', description: 'Face Blowing a Kiss'),
+  Emoji(value: '😗', description: 'Kissing Face'),
+  Emoji(value: '😚', description: 'Kissing Face with Closed Eyes'),
+  Emoji(value: '😙', description: 'Kissing Face with Smiling Eyes'),
+  Emoji(value: '😋', description: 'Face Savoring Food'),
+  Emoji(value: '😛', description: 'Face with Tongue'),
+  Emoji(value: '😜', description: 'Winking Face with Tongue'),
+  Emoji(value: '🤪', description: 'Zany Face'),
+  Emoji(value: '😝', description: 'Squinting Face with Tongue'),
+  Emoji(value: '🤑', description: 'Money-Mouth Face'),
+  Emoji(value: '🤗', description: 'Hugging Face'),
+  Emoji(value: '🤭', description: 'Face with Hand Over Mouth'),
+];
--- flutter_app_7/lib/screens/home_screen.dart
+++ flutter_app_7/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../common/strings.dart' as strings;
+import '../data/emojis.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,8 +15,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late Future<http.Response> futureAlbum;
 
+  int _emojisIndex = 0;
+
   Future<http.Response> fetchAlbum() {
-    return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+    return http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos/1'));
+    // return http.get(Uri.parse(
+    // 'https://open-meteo.com/v1/forecast?latitude=48.8534&longitude=2.3488&hourly=temperature_2m,relativehumidity_2m,precipitation,winddirection_10m,windspeed_10m,cloudcover_2m&timezone=Europe/Paris&current_weather=true&lang=fr'));
   }
 
   @override
@@ -27,27 +32,40 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(strings.homeScreenTitle),
-      ),
-      body: FutureBuilder<http.Response>(
-        future: futureAlbum,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Center(
-              child: Text(snapshot.data!.body),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('${snapshot.error}'),
-            );
-          }
+        appBar: AppBar(
+          title: const Text(strings.homeScreenTitle),
+        ),
+        body: Center(
+          child: Text(
+            '${emojis[_emojisIndex].value} - ${emojis[_emojisIndex].description}',
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+        // body: FutureBuilder<http.Response>(
+        //   future: futureAlbum,
+        //   builder: (context, snapshot) {
+        //     if (snapshot.hasData) {
+        //       return Center(
+        //         child: Text(snapshot.data!.body),
+        //       );
+        //     } else if (snapshot.hasError) {
+        //       return Center(
+        //         child: Text('${snapshot.error}'),
+        //       );
+        //     }
 
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-    );
+        //     return const Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _emojisIndex = (_emojisIndex + 1) % emojis.length;
+            });
+          },
+          child: const Icon(Icons.refresh),
+        ));
   }
 }
